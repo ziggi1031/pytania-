d90d0c881eeded4f6294ff524bce9e123d8f4d8f
ziggi1031 <kamciokw123@gmail.com>
2025-12-25 21:48:08 +0100

    Update serwer.js

--- serwer.js
+++ serwer.js
@@ -1,40 +1,76 @@
+// server.js
 const WebSocket = require('ws');
-const http = require('http');
 
-// Tworzymy serwer HTTP
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>WebSocket Server is running!</h1>');
-});
+const wss = new WebSocket.Server({ port: 8080 });
+console.log("Serwer WebSocket działa na porcie 8080");
 
-// Tworzymy serwer WebSocket
-const wss = new WebSocket.Server({ server });
+let questions = [];
+let currentQuestionIndex = 0;
+let answers = [];
 
-wss.on('connection', (ws) => {
-  console.log('Nowy gracz połączył się!');
-  
-  // Wysyłanie wiadomości powitalnej do nowego gracza
-  ws.send(JSON.stringify({ message: 'Witaj w grze!' }));
+// Funkcja wysyłająca komunikat do wszystkich graczy
+function broadcast(data) {
+  const msg = JSON.stringify(data);
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(msg);
+    }
+  });
+}
 
-  // Odbieranie wiadomości od gracza
-  ws.on('message', (message) => {
-    console.log('Otrzymano wiadomość:', message);
+wss.on('connection', ws => {
+  console.log('Nowy gracz połączony');
 
-    // Rozsyłanie wiadomości do wszystkich połączonych graczy
-    wss.clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+  // Wyślij aktualne pytania i indeks, jeśli gra już trwa
+  if (questions.length > 0) {
+    ws.send(JSON.stringify({
+      type: "SYNC_GAME",
+      questions,
+      currentQuestionIndex,
+      answers
+    }));
+  }
+
+  ws.on('message', message => {
+    const data = JSON.parse(message);
+
+    switch (data.type) {
+
+      // Dodawanie pytania
+      case 'addQuestion':
+        if (!questions.includes(data.text)) {
+          questions.push(data.text);
+          broadcast({ type: 'addQuestion', text: data.text });
+        }
+        break;
+
+      // Start gry
+      case 'startGame':
+        questions = data.questions;
+        currentQuestionIndex = 0;
+        answers = [];
+        broadcast({ type: 'PHASE_QUESTIONS_END', questions });
+        // Wyślij pierwsze pytanie
+        broadcast({ type: 'NEW_QUESTION', question: questions[currentQuestionIndex] });
+        break;
+
+      // Odpowiedź gracza
+      case 'answer':
+        answers.push(data.payload);
+        broadcast({ type: 'answer', payload: data.payload });
+        break;
+
+      // Następne pytanie
+      case 'nextQuestion':
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+          broadcast({ type: 'NEW_QUESTION', question: questions[currentQuestionIndex] });
+        } else {
+          broadcast({ type: 'SHOW_RESULTS' });
+        }
+        break;
+    }
   });
 
-  // Obsługuje zamknięcie połączenia
-  ws.on('close', () => {
-    console.log('Gracz rozłączył się');
-  });
-});
-
-// Uruchomienie serwera na porcie 8080
-server.listen(8080, () => {
-  console.log('Serwer WebSocket działa na porcie 8080');
+  ws.on('close', () => console.log('Gracz rozłączony'));
 });
